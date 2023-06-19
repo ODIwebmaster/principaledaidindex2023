564ea7850348d06af1b41bd6ab480707b218f68f
ODI <43065776+ODIwebmaster@users.noreply.github.com>
2023-06-19 16:45:03 +0100

    Add files via upload

---
+++ js/v0/config.js
@@ -0,0 +1,30 @@
+console.log('loading config')
+
+////update here to amend the current year
+var current_year_configured = "2021";
+
+// $('.current_year_text')
+
+// var year_array = ["2017", "2016", "2015", "2014"]
+var year_array =[];
+var sorted_year_Array;
+d3.csv('data/data.csv', function(data){
+  data.forEach(function(d){
+    if(year_array.includes(d.Year) === false){
+      year_array.push(d.Year)
+    }
+    sorted_year_Array = year_array.sort(function(a, b){
+      return d3.ascending(parseFloat(a), parseFloat(b))
+    })
+  })
+  sorted_year_Array.forEach(function(year, index){
+    var this_button_id = "year_" +year
+    $('#country_profile_buttons').append(
+      '<button type="button" id = "'+this_button_id+'" class="btn btn-secondary year_switch">'+year+'</button>'
+    )
+
+    if(index === sorted_year_Array.length -1){
+      $("#"+this_button_id).addClass('active')
+    }
+  })
+})
